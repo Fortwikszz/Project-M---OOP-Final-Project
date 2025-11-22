d5e4fef2ca4a31193b5a49de072a5744e0ed7ea2
Eatherrr <marvelmahanara39@gmail.com>
2025-11-22 16:53:51 +0700

    testing doang

--- code/main_mape.py
+++ code/main_mape.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import *
+from tiled import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -10,6 +13,18 @@ class Level:
         self.vissible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
         
+        # srite setup
+        self.create_map()
+        
+    def create_map(self):
+        # Create the game mao
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.vissible_sprites])
+                    
     def run(self):
         # Update and draw the game level
-        pass
+        self.vissible_sprites.draw(self.display_surface)
