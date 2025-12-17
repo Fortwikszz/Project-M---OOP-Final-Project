86dcf1a24ac7625218df091ba1dde9b70e0d9107
marvel mahanara <marvelmahanara39@gmail.com>
2025-12-17 16:49:20 +0700

    maps

--- assets/maps/Building.tsx
+++ assets/maps/Building.tsx
@@ -1,2 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Building" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Building" tilewidth="320" tileheight="320" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/House2.png" width="128" height="192"/>
+ </tile>
+ <tile id="1">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/House3.png" width="128" height="192"/>
+ </tile>
+ <tile id="2">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/Monastery.png" width="192" height="320"/>
+ </tile>
+ <tile id="3">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/Tower.png" width="128" height="256"/>
+ </tile>
+ <tile id="4">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/Archery.png" width="192" height="256"/>
+ </tile>
+ <tile id="5">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/Barracks.png" width="192" height="256"/>
+ </tile>
+ <tile id="6">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/Castle.png" width="320" height="256"/>
+ </tile>
+ <tile id="7">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/House1.png" width="128" height="192"/>
+ </tile>
+</tileset>
