2169c5fac9642fdf5bd073781e446fb50a79774f
marvel mahanara <marvelmahanara39@gmail.com>
2025-12-17 19:24:41 +0700

    add boss map

--- assets/maps/Building.tsx
+++ assets/maps/Building.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Building" tilewidth="320" tileheight="320" tilecount="8" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="Building" tilewidth="2944" tileheight="1440" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/House2.png" width="128" height="192"/>
@@ -24,5 +24,17 @@
  </tile>
  <tile id="7">
   <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/House1.png" width="128" height="192"/>
+ </tile>
+ <tile id="8">
+  <image source="C:/Users/ASUS/Downloads/n.png" width="2944" height="1440"/>
+ </tile>
+ <tile id="9">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/1765970649810.png" width="714" height="349"/>
+ </tile>
+ <tile id="10">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/1765972079848.png" width="1280" height="514"/>
+ </tile>
+ <tile id="11">
+  <image source="../Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Black Buildings/1765971597713.png" width="2816" height="912"/>
  </tile>
 </tileset>
